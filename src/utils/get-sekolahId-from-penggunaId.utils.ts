ec5dcd42ecb33afb28ce1e27b995983e4ef8fbac
Faisal Uje <faisal.uje@gmail.com>
2020-04-08 12:08:54 +0700

    bug when start to add absensi manual

--- src/utils/get-sekolahId-from-penggunaId.utils.ts
+++ src/utils/get-sekolahId-from-penggunaId.utils.ts
@@ -1,9 +1,12 @@
 import { Pengguna } from 'src/entities/pengguna.entity'
+import { PenggunaTestGeisa } from 'src/entities/pengguna.testgeisa.entity'
 
 export default async function getSekolahIdFromPenggunaId(
   penggunaId: string,
 ): Promise<string> {
-  const pengguna = await Pengguna.findOne(penggunaId)
+  const pengguna =
+    (await Pengguna.findOne(penggunaId)) ||
+    (await PenggunaTestGeisa.findOne(penggunaId))
   if (pengguna) {
     return pengguna.sekolahId
   } else {
